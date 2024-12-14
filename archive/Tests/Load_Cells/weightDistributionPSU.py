632fcd4d3851de3c063017739978be83b3eea817
LEGARD <legard@legard.com>
2024-12-13 18:15:20 -0700

    new test files

--- archive/Tests/Load_Cells/weightDistributionPSU.py
+++ archive/Tests/Load_Cells/weightDistributionPSU.py
@@ -16,35 +16,23 @@ calibration_ratios = []
 for index, hx in enumerate(sensors):
     hx.zero()
     reading = hx.get_data_mean(readings=100)
-    calibrationWeight = 0.1
+    calibrationWeight = 10
     calibrationValue = float(calibrationWeight)
     ratio = reading / calibrationValue
     hx.set_scale_ratio(ratio)
     calibration_ratios.append(ratio)
     print(f'Sensor {index + 1} calibration ratio set to {ratio}')
 
-# Function to re-map a number from one range to another.
-def scale_to(val, fromLow, fromHigh, toLow, toHigh):
-    return (val - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow
+weights = []
 
-initialWeights = []
-scaledWeights = []
-
-# Get initial weights and scale them
 for index, hx in enumerate(sensors):
-    initialWeight = hx.get_weight_mean()
-    initialWeights.append(initialWeight)
-    scaledWeight = scale_to(initialWeight, initialWeight, 1000, 0, 100)  # Fixed parameters
-    scaledWeights.append(scaledWeight)
+    weight = hx.get_weight_mean()
+    weights.append(weight)
 
-# Print the lengths of scaled and initial weights
-sw = len(scaledWeights)
-iw = len(initialWeights)
-print(f'Length of scaled / initial: {sw} {iw}')
+
 
 while True:
-    print(scaledWeights)  # Print the updated scaled weights for verification
     for i in range(4):  # Corrected range to loop over indices
         weight = sensors[i].get_weight_mean()  # Corrected to use the sensors list
-        scaledWeight = scale_to(weight, initialWeights[i], 1000, 0, 100)  # Fixed parameters
-        scaledWeights[i] = weight  # Update the scaled weight for the current index
+        weights[i] = weight # Update the scaled weight for the current index
+        print(f'{i}: {weight}')  # Print the calculated values
